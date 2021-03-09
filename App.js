e0ec7dc4a3bd07e70e67a4ba6392bcbd86e7f652
E. Hannah <echannah631@gmail.com>
2021-03-09 12:18:04 -0800

    added custom fonts and able to load them asynchronously for usage

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import Header from './components/Header';
 import StartGame from './content/StartGame';
 import Game from './content/Game';
 import GameOver from './content/GameOver';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    montserrat: require('./assets/fonts/Montserrat-Regular.ttf'),
+    'montserrat-bold': require('./assets/fonts/Montserrat-Bold.ttf'),
+  });
+};
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameRounds, setGameRounds] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  if (!loaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
 
   const newGameHandler = () => {
     setGameRounds(0);
