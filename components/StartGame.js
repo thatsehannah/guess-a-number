88992b538f1a5368d3f9fd9b2ae56d36690253f7
E. Hannah <echannah631@gmail.com>
2021-03-09 08:57:23 -0800

    created custom card component with styles

--- components/StartGame.js
+++ components/StartGame.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+import Card from './Card';
 
 const StartGame = (props) => {
-  return <View style={styles.screen}>
-    <Text>Game Screen</Text>
-  </View>;
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start A New Game</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select A Number</Text>
+        <TextInput />
+        <View style={styles.buttonsContainer}>
+          <View style={styles.button}>
+            <Button title='RESET' />
+          </View>
+          <View style={styles.button}>
+            <Button title='CONFIRM' />
+          </View>
+        </View>
+      </Card>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -12,6 +28,24 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: '43%',
   },
 });
 
