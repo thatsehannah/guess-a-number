0e2dfc353d137cc83c9a8e38445155b89451f347
E. Hannah <echannah631@gmail.com>
2021-03-09 08:32:27 -0800

    created start game component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header'
+import StartGame from './components/StartGame'
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess A Number" />
+      <StartGame />
     </View>
   );
 }
