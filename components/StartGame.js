767a8fbfdd455b0a979384c8b76f133cb11661ed
E. Hannah <echannah631@gmail.com>
2021-03-09 09:25:54 -0800

    created custom input component, created constant file for color reusage

--- components/StartGame.js
+++ components/StartGame.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import colors from '../constants/colors';
 
 import Card from './Card';
+import Input from './Input';
 
 const StartGame = (props) => {
   return (
@@ -9,13 +11,20 @@ const StartGame = (props) => {
       <Text style={styles.title}>Start A New Game</Text>
       <Card style={styles.inputContainer}>
         <Text>Select A Number</Text>
-        <TextInput />
+        <Input
+          blurOnSubmit
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='number-pad'
+          maxLength={2}
+          style={styles.input}
+        />
         <View style={styles.buttonsContainer}>
           <View style={styles.button}>
-            <Button title='RESET' />
+            <Button title='RESET' color={colors.secondary} />
           </View>
           <View style={styles.button}>
-            <Button title='CONFIRM' />
+            <Button title='CONFIRM' color={colors.primary} />
           </View>
         </View>
       </Card>
@@ -46,6 +55,10 @@ const styles = StyleSheet.create({
   },
   button: {
     width: '43%',
+  },
+  input: {
+    width: '20%',
+    textAlign: 'center',
   },
 });
 
