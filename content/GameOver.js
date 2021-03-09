b7bd3111013da48ab47117220d325e615aefe206
E. Hannah <echannah631@gmail.com>
2021-03-09 13:13:13 -0800

    styled imaged on game over screen

--- content/GameOver.js
+++ content/GameOver.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button, Image } from 'react-native';
 import colors from '../constants/colors';
-import BodyText from '../components/BodyText'
-import TitleText from '../components/TitleText'
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const GameOver = (props) => {
-  const {rounds, numberGuessed, newGame} = props
+  const { rounds, numberGuessed, newGame } = props;
   return (
     <View style={styles.screen}>
       <TitleText>The game is over!</TitleText>
-      <Image source={require('../assets/images/success.png')}/>
+      <Image
+        style={styles.image}
+        source={require('../assets/images/success.png')}
+        resizeMode='cover'
+      />
       <BodyText>Number of rounds: {rounds}</BodyText>
       <BodyText>Number was: {numberGuessed}</BodyText>
-      <Button title='START OVER' color={colors.secondary} onPress={newGame}></Button>
+      <Button
+        title='START OVER'
+        color={colors.secondary}
+        onPress={newGame}
+      ></Button>
     </View>
   );
 };
@@ -22,6 +30,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: '#000',
+    marginVertical: 30
   },
 });
 
